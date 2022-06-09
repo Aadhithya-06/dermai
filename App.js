e63d62c642b99b660fadf9aea5e592d60a367ee8
id-aaryan <am5020@imperial.ac.uk>
2022-06-09 17:46:58 +0100

    change to old app.js

--- App.js
+++ App.js
@@ -1,38 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './screens/HomeScreen.js';
-import StackNavigator from './StackNavigator.js';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-
-
-//import {Provider} from "react-redux";
-
-const MyTheme = {
-  dark: false,
-  colors: {
-    primary: 'rgb(255, 45, 85)',
-    background: 'rgb(255, 255, 255)',
-    card: 'rgb(255, 255, 255)',
-    text: 'rgb(28, 28, 30)',
-    border: 'rgb(199, 199, 204)',
-    notification: 'rgb(255, 69, 58)',
-  },
-};
+import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <NavigationContainer theme={MyTheme}>
-        <StackNavigator />
-    </NavigationContainer>
-
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Universal React with Expo</Text>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
