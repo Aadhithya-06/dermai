92d7a4256b1d53869c11b4f4ad51e110c91e1251
Sidharth Jain <sidharthjain20022@gmail.com>
2022-06-14 15:23:10 +0100

    Welcome page done

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import RootStack from './navigators/RootStack'
 
+import Welcome from './screens/Welcome'
+
 export default function App() {
-  return <RootStack/>;
+  return <Welcome/>;
 }
