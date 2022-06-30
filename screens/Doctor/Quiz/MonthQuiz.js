6fe0b4605b73f828387936e41acbbce7a4bb0a09
id-aaryan <am5020@imperial.ac.uk>
2022-06-30 20:50:35 +0100

    change to monthly quiz

--- screens/Doctor/Quiz/MonthQuiz.js
+++ screens/Doctor/Quiz/MonthQuiz.js
@@ -1,88 +1,88 @@
-import { StyleSheet, Text, View, TouchableOpacity,Image, ImageEditor, Alert } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Image,TextInput, Alert} from 'react-native'
 import React, {useState,useEffect} from 'react'
 import tw from 'tailwind-react-native-classnames'
-
-
 import axios from 'axios'
-import { CardStyleInterpolators } from '@react-navigation/stack'
 import {Fontisto, Entypo, Ionicons} from "@expo/vector-icons"
+import { Formik } from 'formik';
+
+import {
+    StyledContainer,
+    InnerContainer,
+    PageLogoLogin,
+    StyledFormArea,
+    StyledInputLabel,
+    LeftIcon,
+    RightIcon,
+    StyledTextInput,
+    StyledButton,
+    ButtonText,
+    Colors,
+    MsgBox,
+    ExtraText,
+    ExtraView,
+    TextLink,
+    TextLinkContent
+} from '../../../components/styles'
+
+const MonthQuiz = ({navigation, route}) => {
+
+    const [images, setImages] = useState(null);
+    const [images2, setImages2] = useState(null);
+    const [quizType, setQuizType] = useState(null);
+    const [marks, setMarks] = useState(0);
+    const [questions, setQuestions] = useState(0);
+    const [condition, setConditions] = useState(0);
+    // const {name, email} = route.params
+  
+    const getPicture = async () => {
+               const url = 'https://secure-forest-32038.herokuapp.com/excema'
+               axios.post(url).then((response) =>{
+                 const result = response.data;
+                 const {message, status, image1, image2, quizType, condition} = result;
+                 if (status !== "SUCCESS"){
+                    console.log(message)
+                 } else{
+                   setImages(image1)
+                   setImages2(image2);
+                   setQuizType(quizType);
+                   setConditions(condition);
+                }
+             })
+           }
+
+    useEffect(() => {
+         getPicture();
+    }, [questions])
 
 
-const MonthQuiz = ({navigation,route}) => {
-
-  const [images, setImages] = useState(null);
-  const [images2, setImages2] = useState(null);
-  const [quizType, setQuizType] = useState(null);
-  const [marks, setMarks] = useState(0);
-  const [questions, setQuestions] = useState(0);
-  const [condition, setConditions] = useState(0);
-  // const {name, email} = route.params
-
-  const getPicture = async () => {
-             const url = 'https://secure-forest-32038.herokuapp.com/excema'
-             axios.post(url).then((response) =>{
-               const result = response.data;
-               const {message, status, image1, image2, quizType, condition} = result;
-               if (status !== "SUCCESS"){
-                  console.log(message)
-               } else{
-                 setImages(image1)
-                 setImages2(image2);
-                 setQuizType(quizType);
-                 setConditions(condition);
-              }
-           })
-         }
-
-  const hookMethod = async () => {
-    setQuestions(questions+1);
-    if(questions > 4) {
-      // const res = marks;
-      // const q = questions;
-      const credentials = {score: String(marks)}
-      const url = 'https://secure-forest-32038.herokuapp.com/quizSubmit'
-      axios.post(url,credentials).then(()=>{}).catch(error => {});
-      setQuestions(0);
-      setMarks(0);
-      navigation.navigate("DoctorStats", route.params);
-    }
-  }
-
-   useEffect(() => {
-          Alert.alert("Quiz Rules", "If you have the correct answer, we will go straight to the next questions. If you get a question wrong, you will be told what the correct answer is.");
-          getPicture();
-      }, [])
-
-  // if quiztype is 0: what condition is it?
-  // if quiztype is 1: are these the same condition?
-
-    return ( 
-      <View>
-        <Text style={tw `font-bold text-center py-28 text-4xl`}>What condition are these? {questions}/5</Text>
+      return (
         <View style={{justifyContent: 'center', alignItems: 'center'}}>
-          <View style={{bottom: 50, width: 375, height: 500}}>
-              <Image  style={{width: "48%", height: '50%', top:40, left: 0}}
-                      source={{uri: images}}/>
-              <Image  style={{width: "48%", height: '50%', top:-210, left: 190}}
-                      source={{uri: images2}}/>
-          </View>  
-          
+        <Text style={tw `font-bold text-center py-28 text-4xl`}>What condition is this? {questions}/5</Text>
 
-        <View style={styles.textAreaContainer} >
+        <View style={{bottom: 100,  backgroundColor: '#FFFFFF', width: 325, height: 500}}>
+            <Image style={{width: "60%", height: '45%', top:0, left: 60}}
+                    source={{uri: images}}/>
+        </View>  
+
+        <View style={styles.textAreaContainer}>
         <Formik
                 initialValues={{diagnosis: ''}}
                 onSubmit={(values) => {
                     if(values.diagnosis == '') {
                         Alert.alert("We cannot have an empty field", 'Please fill the diagnosis');
                     } else {
-                        setDiagnosis(values.diagnosis);
-                        sendDiagnosis()
-
+                      if (condition == 0) {
+                        if(values.diagnosis.toLowerCase=="excema") {
+                          setMarks(marks+1);
+                        }
+                        setQuestions(questions + 1);
+                      }
+                      
                     }
                 }}
             >{({handleChange,handleBlur,handleSubmit, values}) => (<StyledFormArea>
                 <MyTextInput
-                    placeholder = "Enter diagnosis of skin disease"
+                    placeholder = "Enter information about your observations"
                     placeholderTextColor={'#9CA3AF'}
                     onChangeText={handleChange('diagnosis')}
                     onBlur={handleBlur('diagnosis')}
@@ -91,47 +91,56 @@ const MonthQuiz = ({navigation,route}) => {
                 <StyledButton style={styles.buttons} 
                                   onPress={handleSubmit}>
                     <ButtonText>
-                        Send diagnosis
+                        Submit Answer
                     </ButtonText>
                 </StyledButton>
             </StyledFormArea>)}
 
             </Formik>
-        </View> 
+        </View>
 
-
-
-
-            {/* <TouchableOpacity style={{bottom: 290, left:80,  backgroundColor: '#000000', width: 150, height: 50, borderRadius:20}}
-                             onPress={() => {
-                            //   hookMethod();
-                            //   if (condition == 0) {
-                            //     Alert.alert("Incorrect Answer", "The answer is Eczema");
-                            //   } else if (condition == 1){
-                            //     Alert.alert("Incorrect Answer", "The answer is Psoriasis");
-                            //   } else {
-                            //     Alert.alert("Incorrect Answer", "The answer is Keloids");
-                            //   }
-                            //   getPicture();
-                             }}>
-              <Text style={tw `font-semibold text-center text-2xl text-white`}>Other</Text>
-            </TouchableOpacity>  */}
-
-            <TouchableOpacity style={[tw `absolute left-8 items-center`, {left: 0,right: 0,top:-35}]}
-                        onPress={() => navigation.navigate("DoctorLanding",route.params)}>
+        <View style={{bottom:50}}>
+            <TouchableOpacity style={styles.button}
+                onPress={() => navigation.navigate("QuizLanding",route.params)}>
           <Entypo name="home" size={45} /> 
-          <Text style={tw `font-semibold`}>Main Menu</Text>
+          <Text style={[tw `font-semibold right-3`]}>Main Menu</Text>
         </TouchableOpacity>  
-          </View>
-  
-       </View>
+        </View>
+        </View>
+      )
 
+}
 
-   
-    )
-  
+const styles = StyleSheet.create({
+    textAreaContainer: {
+      borderColor: "#000000",
+      borderWidth: 2,
+      padding: 5,
+      width: 350,
+      top: -350
+    },
+    textArea: {
+      height: 50,
+      justifyContent: "flex-start"
+    },
+    button:{
+        top:-115,
+        alignContent:'center',
+        left: 10
+    },
+    buttons:{
+        top: 100,
+        left: 10
+    }
+  })
+
+  const MyTextInput = ({label, icon,isPassword,hidePassword,setHidePassword, ...props}) => {
+    return(
+        <View style={styles.textArea}>
+            <LeftIcon>
+            </LeftIcon>
+            <TextInput {...props}/>
+        </View>);
 }
 
 export default MonthQuiz
-
-const styles = StyleSheet.create({})
